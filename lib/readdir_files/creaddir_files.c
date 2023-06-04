529a3abbcac1004c2867c82db04bead103fb79cc
haller33 <eliseu42lucena@gmail.com>
2023-06-03 23:03:16 -0300

    add linear reading of files with offset with the max size of file name

--- lib/readdir_files/creaddir_files.c
+++ lib/readdir_files/creaddir_files.c
@@ -14,6 +14,7 @@
 // when used build_c_test.sh need this HAVE_MAIN to be true
 //
 #define HAVE_MAIN false
+#define HAVE_MAIN_LINEAR true
 #define MAX_CARACTER_OF_FILE 64
 #define MAX_BYTES_FILE_NAME sizeof(char) * MAX_CARACTER_OF_FILE
 
@@ -24,6 +25,16 @@ int debug_read_dir_files(char **files_arr_ret, int32_t size_files) {
   for (i = 0; i < size_files; i++) {
     printf("%s\n", files_arr_ret[i]);
   }
+
+  return 0;
+}
+
+
+int free_read_dir_linear(char *files_arr_ret, int32_t size_files) {
+
+  int32_t i;
+
+  free(files_arr_ret);
 
   return 0;
 }
@@ -98,6 +109,7 @@ int creaddir_files(char *name_dir, char ***files_arr_ret,
   for (i = 0; i < files_count; i++) {
 
     files_arr[i] = (char *)calloc(sizeof(char *), MAX_CARACTER_OF_FILE);
+
     // memset(files_arr_ret[i][0], 0, MAX_BYTES_FILE_NAME); // no need because
     // calloc
   }
@@ -136,8 +148,66 @@ int creaddir_files(char *name_dir, char ***files_arr_ret,
   return (0);
 }
 
+int creaddir_files_linear(char *name_dir, char **files_arr_ret,
+                   int32_t *size_of_dir_t) {
+
+  DIR *folder;
+  struct dirent *entry;
+  int32_t files_count = 0;
+  int32_t i = 0;
+  char *files_arr = NULL;
+  // char* tmp_str = NULL;
+
+  files_count = count_files_dir(name_dir);
+
+  // directory do not exist
+  if (files_count == -4)
+    return -4;
+
+  if (files_count == 0)
+    return -2;
+
+  assert(files_count > -1);
+
+  // not include . .. folders on list, that a ready have count on
+  // count_files_dir()
+  files_arr = (char *)calloc(sizeof(char *), files_count * MAX_CARACTER_OF_FILE);
+
+  folder = opendir(name_dir);
+  if (folder == NULL) {
+    perror("Unable to read directory");
+    return (1);
+  }
+
+  i = 0;
+  while ((entry = readdir(folder))) {
+
+#if false
+    if (i == 3017) {
+      // tmp_str = strdup(entry->d_name);
+      printf("Type REG :: %d\n", entry->d_type == DT_REG);
+      printf("Type :: %ld\n", entry->d_off);
+      printf("name :#: %s\n", entry->d_name);
+      printf("count :: %d", i);
+    }
+#endif
+    if (!(entry->d_type == DT_DIR) &&
+        (entry->d_off > 2)) { // ignore default linux directory [. ..]
+        strcpy(&files_arr[i * MAX_CARACTER_OF_FILE], entry->d_name);
+      i = i + 1;
+    }
+  }
+
+  *files_arr_ret = files_arr;
+  *size_of_dir_t = files_count;
+
+  closedir(folder);
+
+  return (0);
+}
+
 #if HAVE_MAIN
-int main() {
+int main_old() {
 
   char **files_arr = NULL;
   char *directory = "/bin";
@@ -187,3 +257,59 @@ int main() {
   return 0;
 }
 #endif // HAVE_MAIN
+
+
+
+#if HAVE_MAIN_LINEAR || HAVE_MAIN
+int main() {
+
+  char *files_arr = NULL;
+  char *directory = "/bin";
+  // char *directory =
+  // "/home/synbian/git/clone/Odin/spawn-rune/readdir_files/test";
+
+  int32_t size_files = 0;
+  int32_t i = 0;
+
+  creaddir_files_linear(directory, &files_arr, &size_files);
+
+#if true // to test performance of readding and free memory on C
+  for (i = 0; i < size_files; i++) {
+
+#if false
+    if (strcmp("", files_arr[i]) == 0) {
+      printf("|| %d", i);
+      printf("%s\n", files_arr[i]);
+      break;
+    }
+#endif
+#if false
+    if (i == 5017) {
+      printf("|| %d", i);
+      printf("%s\n", files_arr[i]);
+      printf("---------\n");
+    }
+#endif
+    // printf("file(%d) : %s\n", i, files_arr[i]);
+    // if(files_arr[i] == NULL) continue;
+    printf("%s\n", &files_arr[i*MAX_CARACTER_OF_FILE]);
+  }
+#endif
+
+  // printf("# %d\n", size_files);
+
+#if false
+  for (i = 0; i < size_files; i++) {
+    free(files_arr[i]);
+  }
+
+  free(files_arr);
+#endif
+
+  #if true
+  free_read_dir_linear(files_arr, size_files);
+  #endif
+
+  return 0;
+}
+#endif // HAVE_MAIN_LINEAR
